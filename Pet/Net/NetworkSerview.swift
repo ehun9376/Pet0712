5bf2578babd5b40dca35ab60ad7b74de05a94b6c
陳逸煌 <ehun9376@gmail.com>
2022-07-14 17:57:12 +0800

    update

--- Pet/Net/NetworkSerview.swift
+++ Pet/Net/NetworkSerview.swift
@@ -13,20 +13,19 @@ enum PetType {
 }
 // 網路層
 class NetworkService {
-    static func downloadJson(type: PetType, completed: @escaping (Result<[PetModel], Error>)->()) {
+    static func downloadJson(type: PetType, completed: ((Result<[PetModel], Error>) -> ())? = nil) {
         let _type: String = type == .dog ? "%E7%8B%97" : "%E8%B2%93"
-        //type怎麼直接可以讀到
         let url = URL(string: "https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL&$top=20&$skip=0&animal_kind=" + _type)
         URLSession.shared.dataTask(with: url!) { data, response, error in
-            guard error == nil,let data = data else {
-                print("DownloadWrong")
-                return
+
+            if let error = error {
+                completed?(.failure(error))
             }
+            guard let data = data else { return }
             do{
                 let pets = try JSONDecoder().decode([PetModel].self, from: data)
-                DispatchQueue.main.async {
-                    completed(.success(pets))
-                }
+                completed?(.success(pets))
+                
             } catch {
                 print("JsonDecoderWrong")
             }
